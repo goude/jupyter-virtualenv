8b2a8eea1409a4aa32ea6a77c540c254261b7eb5
Daniel Goude <daniel@goude.se>
2016-11-19 17:26:12 +0100

    update setup, add jupyter extensions, vim key bindings, etc

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pyvenv venv
 
 source venv/bin/activate
 
-npm install -g configurable-http-proxy
+npm install -g configurable-http-proxy # jupyterhub requirement
 
 pip3 install numpy
 pip3 install sympy scipy seaborn pandas jupyter
@@ -14,6 +14,25 @@ pip3 install bokeh
 pip3 install scikit-learn
 pip3 install jupyterhub
 
-pip3 install bravado # Swagger client
+# swagger client
+pip3 install bravado
+
+# TODO: Using --py and --sys-prefix, due to error message in bokeh example. Check why this is needed.
+
+# interactive widgets, see bokeh example
+pip3 install ipywidgets
+jupyter nbextension enable --py --sys-prefix widgetsnbextension --user
+
+# recommended by vim key bindings for easier setup - https://github.com/ipython-contrib/jupyter_contrib_nbextensions#installation
+pip3 install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --py --sys-prefix
+
+pip3 install jupyter_nbextensions_configurator
+jupyter nbextensions_configurator enable --py --sys-prefix
+
+# vim key bindings - https://github.com/lambdalisue/jupyter-vim-binding/wiki/Installation
+git clone https://github.com/lambdalisue/jupyter-vim-binding $(jupyter --data-dir)/nbextensions/vim_binding
+jupyter nbextension enable --py --sys-prefix vim_binding/vim_binding
+#chmod -R go-w vim_binding
 
 deactivate
