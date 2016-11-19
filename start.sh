9565b60ed764b861cb81177189417756c88e12d8
Daniel Goude <daniel@goude.se>
2016-11-19 15:17:58 +0100

    make more portable

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-source venv/bin/activate
-cd data
+JUPYTER_VENV_DIR=$HOME/jupyter-virtualenv
+JUPYTER_VENV_DATA_DIR=$JUPYTER_VENV_DIR/data
+
+source $JUPYTER_VENV_DIR/venv/bin/activate
+cd $JUPYTER_VENV_DATA_DIR
+
 jupyter notebook --ip='*' --no-browser
