8217131e7f2b69c6d0344c92b56a18fe5417d567
Daniel Goude <daniel@goude.se>
2016-12-01 22:31:24 +0000

    add theme to start script

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -5,4 +5,8 @@ JUPYTER_VENV_DATA_DIR=$JUPYTER_VENV_DIR/data
 source $JUPYTER_VENV_DIR/venv/bin/activate
 cd $JUPYTER_VENV_DATA_DIR
 
+echo "Setting theme..."
+jt -f inputmono -fs 12 -nf opensans -tf rasaserif -tfs 18 -vim -t grade3 -cellw 1100
+
+echo "Starting Jupyter..."
 jupyter notebook --ip='*' --no-browser
