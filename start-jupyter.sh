c52a61a4d3ff2b8b169abc57b15a632869a4c123
Daniel Goude <daniel@goude.se>
2016-12-01 22:51:25 +0000

    very cheap theming color fix

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -8,5 +8,8 @@ cd $JUPYTER_VENV_DATA_DIR
 echo "Setting theme..."
 jt -f inputmono -fs 12 -nf opensans -tf rasaserif -tfs 18 -vim -t grade3 -cellw 1100
 
+echo "Applying [fulhack] colorfix..."
+sed -i.bak 's/efefef/f8f8f8/g' ~/.jupyter/custom/custom.css
+
 echo "Starting Jupyter..."
 jupyter notebook --ip='*' --no-browser
