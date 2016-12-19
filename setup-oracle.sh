ffbff5cf0ffe2c5c468d5dc11d30596ed055f697
Daniel Goude <daniel@goude.se>
2016-12-19 13:18:08 +0000

    update oracle config

--- setup-oracle.sh
+++ setup-oracle.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 source venv/bin/activate
+source oracle-environment
+
 PIP_CMD="pip3 --no-cache-dir"
 
+# Sorry, but you need to download the installation files manually
 sudo alien -i install/oracle-instantclient12.1-basiclite-12.1.0.2.0-1.x86_64.rpm
 sudo alien -i install/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm
-
-export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client/lib
-export ORACLE_HOME=/usr/lib/oracle/11.2/client
-export TNS_ADMIN=/usr/lib/oracle/11.2/client/network/admin
 
 $PIP_CMD install cx_oracle
