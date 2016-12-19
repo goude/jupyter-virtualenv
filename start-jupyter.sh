820b22ae1299ede3af7b9ac27dc9154c113c94f0
Daniel Goude <daniel@goude.se>
2016-12-19 14:28:15 +0000

    ud font sizes

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -6,7 +6,7 @@ source $JUPYTER_VENV_DIR/venv/bin/activate
 cd $JUPYTER_VENV_DATA_DIR
 
 echo "Setting theme..."
-jt -f inputmono -fs 12 -nf opensans -tf rasaserif -tfs 16 -vim -t grade3 -cellw 1100
+jt -f inputmono -fs 10 -nf opensans -nfs 12 -tf rasaserif -tfs 14 -vim -t grade3 -cellw 1100
 
 echo "Applying [fulhack] colorfix..."
 sed -i.bak 's/efefef/f8f8f8/g' ~/.jupyter/custom/custom.css
