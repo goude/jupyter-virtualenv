23463e8d9a6204d04a43b14098b7cb83fafef09a
Daniel Goude <daniel@goude.se>
2017-05-06 18:04:16 +0200

    fix path

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#JUPYTER_VENV_DIR=$HOME/jupyter-virtualenv
-#JUPYTER_VENV_DATA_DIR=$JUPYTER_VENV_DIR/data
-JUPYTER_VENV_DATA_DIR=./data
+JUPYTER_VENV_DIR=$HOME/jupyter-virtualenv
+JUPYTER_VENV_DATA_DIR=$JUPYTER_VENV_DIR/data
 
 #source $JUPYTER_VENV_DIR/venv/bin/activate
 eval "$(pyenv init -)"
