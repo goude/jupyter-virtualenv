77e7e477f1ff71c188b0b52578114f91556631a2
Daniel Goude <daniel@goude.se>
2017-09-25 14:20:54 +0200

    update start script

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-JUPYTER_VENV_DIR=$HOME/jupyter-virtualenv
-JUPYTER_VENV_DATA_DIR=$JUPYTER_VENV_DIR/data
+#!/usr/bin/env bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+JUPYTER_VENV_DATA_DIR=$SCRIPT_DIR/data
 
-#source $JUPYTER_VENV_DIR/venv/bin/activate
 eval "$(pyenv init -)"
 pyenv activate pyenv-jupyter
 
@@ -20,13 +19,12 @@ if [ $# -ne 0 ]
     jupyter nbextension enable vim_binding/vim_binding --sys-prefix
 
     echo "Setting theme..."
-    jt -f firacode -fs 12 -nf robotosans -nfs 12 -tf loraserif -tfs 14 -vim -t grade3 -cellw 1100
+    #jt -f firacode -fs 12 -nf robotosans -nfs 12 -tf loraserif -tfs 14 -vim -t grade3 -cellw 1100
+    jt -vim -t grade3 -f firacode -fs 11 -tfs 11 -nfs 11 -cellw 88% -T
 
     echo "Applying [fulhack] colorfix..."
     sed -i.bak 's/efefef/f8f8f8/g' ~/.jupyter/custom/custom.css
 
-    # for now, revert the theme changes
-    jt -r # reset jt
 fi
 
 echo "Starting Jupyter..."
