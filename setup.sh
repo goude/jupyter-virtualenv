1a88c48f7c28ede30b297b8fb9401ba85c038419
Daniel Goude <daniel@goude.se>
2017-10-02 00:17:30 +0200

    Restructure setup, add jupyterlab

--- setup.sh
+++ setup.sh
@@ -13,30 +13,14 @@ pyenv activate pyenv-jupyter
 PIP_CMD="pip3 --no-cache-dir"
 $PIP_CMD install --upgrade pip
 
-$PIP_CMD install numpy
-$PIP_CMD install sympy scipy seaborn pandas jupyter
-$PIP_CMD install bokeh
-$PIP_CMD install scikit-learn
-$PIP_CMD install tensorflow
-$PIP_CMD install yapf # python formatting
-$PIP_CMD install pillow # imaging
-$PIP_CMD install requests # imaging
-$PIP_CMD install folium # maps
-$PIP_CMD install sas7bdat # SAS reader
-$PIP_CMD install geocoder
-$PIP_CMD install pyfiglet # ascii banners
-$PIP_CMD install matplotlib-venn
-$PIP_CMD install plotly
-$PIP_CMD install wordcloud
-$PIP_CMD install nltk # natural language toolkit
-$PIP_CMD install vincent
-$PIP_CMD install pyfiglet
-$PIP_CMD install jaydebeapi
-$PIP_CMD install openpyxl
-$PIP_CMD install arrow # datetime replacement
-$PIP_CMD install psycopg2
-$PIP_CMD install sqlalchemy
-$PIP_CMD install lesscpy # jupyter warned about this missing package
+$PIP_CMD install numpy sympy scipy seaborn pandas jupyter \
+bokeh scikit-learn tensorflow yapf \
+pillow requests folium sas7bdat geocoder \
+pyfiglet matplotlib-venn plotly wordcloud \
+nltk vincent pyfiglet jaydebeapi openpyxl \
+arrow psycopg2 sqlalchemy lesscpy ipywidgets \
+jupyter_nbextensions_configurator jupyter_contrib_nbextensions \
+jupyterthemes jupyterlab
 
 #$PIP_CMD install bravado # swagger client
 #$PIP_CMD install datapackage
@@ -44,8 +28,6 @@ $PIP_CMD install lesscpy # jupyter warned about this missing package
 #$PIP_CMD install jsontableschema-pandas
 #$PIP_CMD install jellyfish # phonetic/approximate string matching
 #$PIP_CMD install gensim
-#$PIP_CMD install jupyterhub
-#npm install -g configurable-http-proxy # jupyterhub requirement
 
 # jupyter stuff
 $PIP_CMD install bash_kernel
@@ -55,24 +37,19 @@ python -m bash_kernel.install
 pyenv rehash
 
 # interactive widgets, see bokeh example
-$PIP_CMD install ipywidgets
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 
 # recommended by vim key bindings for easier setup - https://github.com/ipython-contrib/jupyter_contrib_nbextensions#installation
-$PIP_CMD install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --sys-prefix
-
-$PIP_CMD install jupyter_nbextensions_configurator
 jupyter nbextensions_configurator enable --sys-prefix
 
-# https://github.com/dunovank/jupyter-themes
-$PIP_CMD install jupyterthemes
+# alpha version of jupyterlab
+jupyter serverextension enable --py jupyterlab --sys-prefix
 
 # vim key bindings - https://github.com/lambdalisue/jupyter-vim-binding/wiki/Installation
 mkdir -p "$(jupyter --data-dir)/nbextensions"
 cd "$(jupyter --data-dir)/nbextensions" || exit
 git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
-
 # vim key bindings now enabled via start script
 #jupyter nbextension enable vim_binding/vim_binding --sys-prefix
 
