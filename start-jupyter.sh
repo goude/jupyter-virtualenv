7509e82e313f3e5789561667659237e8ea4d38cb
Daniel Goude <daniel@goude.se>
2018-01-22 15:10:35 +0100

    test fix for pyenv issues

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -2,7 +2,14 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 JUPYTER_VENV_DATA_DIR=$SCRIPT_DIR/data
 
-eval "$(pyenv init -)"
+# Test: fix pyenv
+if [[ -d $HOME/.pyenv/bin ]]; then
+  export PATH="$HOME/.pyenv/bin:$PATH"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+fi
+
+#eval "$(pyenv init -)"
 pyenv activate pyenv-jupyter
 
 cd $JUPYTER_VENV_DATA_DIR
