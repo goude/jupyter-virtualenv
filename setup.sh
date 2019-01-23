67bcfd998189c2d029b5fa079f9e76fb44dbba64
Daniel Goude <daniel@goude.se>
2019-01-23 14:11:18 +0100

    minimal version, checking build capability

--- setup.sh
+++ setup.sh
@@ -8,58 +8,66 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
 source "$HOME/.homesick/repos/runcom/localenv"
-pyenv virtualenv "$RUNCOM_PYENV_PYTHON3_VERSION" pyenv-jupyter
+pyenv install --skip-existing 3.6.6
+pyenv virtualenv-delete --force pyenv-jupyter
+pyenv virtualenv 3.6.6 pyenv-jupyter
 pyenv activate pyenv-jupyter
 
 PIP_CMD="pip3 --no-cache-dir install --upgrade"
-$PIP_CMD pip
+$PIP_CMD pip==18
 
 #jupyterlab\>=0.33.0,\<0.33.99 \
+# bqplot ipythonblocks \
+#folium \
+#geocoder \
+#ipyvolume \
+#arrow \
+#autopep8 \
+#bokeh \
+#click \
+#flake8 \
+#keras \
+#lesscpy \
+#matplotlib-venn \
+#neovim \
+#pandas_datareader \
+#pillow \
+#plotly \
+#pyfiglet \
+#tensorflow \
+#vim-vint \
+#vincent \
+#wordcloud \
+#ipywidgets \
+#jupyter_contrib_nbextensions \
+#jupyter_nbextensions_configurator \
+#jedi \
+#nltk \
+#qgrid \
+#regex \
+#scikit-learn \
+#sqlalchemy \
+#yamllint \
+#jupyterthemes \
+#yapf
+#numpy \
+##sympy \
+#scipy \
+#seaborn \
+#pandas \
+#jupyter \
+#jaydebeapi \
+#openpyxl \
+#pytest \
+#sas7bdat \
+#tqdm \
+#xlrd \
 
-$PIP_CMD numpy sympy scipy seaborn pandas jupyter \
-arrow \
-autopep8 \
-bokeh \
-bqplot ipythonblocks \
-click \
-flake8 \
-folium \
-geocoder \
-ipyvolume \
-ipywidgets \
-jaydebeapi \
-jedi \
-jupyter_contrib_nbextensions \
-jupyter_nbextensions_configurator \
-jupyterlab \
-jupyterthemes \
-keras \
-lesscpy \
-matplotlib-venn \
-neovim \
-nltk \
-openpyxl \
-pandas_datareader \
-pillow \
-plotly \
+$PIP_CMD \
 psycopg2-binary \
-pyfiglet \
-pyfiglet \
-pytest \
-qgrid \
-regex \
 requests \
-sas7bdat \
-scikit-learn \
-sqlalchemy \
-tensorflow \
-tqdm \
-vim-vint \
-vincent \
-wordcloud \
-xlrd \
-yamllint \
-yapf
+jupyterlab \
+pandas
 
 #bravado # swagger client
 #datapackage
@@ -67,31 +75,31 @@ yapf
 #jellyfish # phonetic/approximate string matching
 #gensim
 
-$PIP_CMD html5lib
+#$PIP_CMD html5lib
 
 # jupyter stuff
-$PIP_CMD bash_kernel
-python -m bash_kernel.install
+#$PIP_CMD bash_kernel
+#python -m bash_kernel.install
 
 # Important: rehash pyenv to make jupyter command available
 pyenv rehash
 
 # interactive widgets, see bokeh example
-jupyter nbextension enable --py widgetsnbextension --sys-prefix
+#jupyter nbextension enable --py widgetsnbextension --sys-prefix
 
 # qgrid
-jupyter nbextension enable --py --sys-prefix qgrid
+#jupyter nbextension enable --py --sys-prefix qgrid
 
 # recommended by vim key bindings for easier setup - https://github.com/ipython-contrib/jupyter_contrib_nbextensions#installation
-jupyter contrib nbextension install --sys-prefix
-jupyter nbextensions_configurator enable --sys-prefix
+#jupyter contrib nbextension install --sys-prefix
+#jupyter nbextensions_configurator enable --sys-prefix
 
 # alpha version of jupyterlab
 jupyter serverextension enable --py jupyterlab --sys-prefix
 
 jupyter labextension install jupyterlab_vim
 
-jupyter labextension install @mflevine/jupyterlab_html
+#jupyter labextension install @mflevine/jupyterlab_html
 #jupyter labextension install @jupyterlab/latex
 #jupyter labextension install @jpmorganchase/perspective-jupyterlab
 #jupyter labextension install @jupyter-widgets/jupyterlab-manager
@@ -117,13 +125,13 @@ jupyter labextension install @mflevine/jupyterlab_html
 
 # vim key bindings - https://github.com/lambdalisue/jupyter-vim-binding/wiki/Installation
 # enabled via start script (jupyter nbextension enable vim_binding/vim_binding --sys-prefix)
-mkdir -p "$(jupyter --data-dir)/nbextensions"
-cd "$(jupyter --data-dir)/nbextensions" || exit
-if [ -d vim_binding ]; then
-  rm -rf vim_binding
-fi
-git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
-cd - || exit
+#mkdir -p "$(jupyter --data-dir)/nbextensions"
+#cd "$(jupyter --data-dir)/nbextensions" || exit
+#if [ -d vim_binding ]; then
+  #rm -rf vim_binding
+#fi
+#git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+#cd - || exit
 
 # ...and we're done.
 pyenv rehash
