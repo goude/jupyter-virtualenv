cb8bbf42aec650cb87bea4e52fcb89f38e555921
Daniel Goude <daniel@goude.se>
2019-03-23 12:18:51 +0100

    use packages for setup.sh

--- setup.sh
+++ setup.sh
@@ -8,35 +8,20 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
 source "$HOME/.homesick/repos/runcom/localenv"
-pyenv install --skip-existing 3.6.6
+pyenv install --skip-existing 3.6.7
 pyenv virtualenv-delete --force pyenv-jupyter
-pyenv virtualenv 3.6.6 pyenv-jupyter
+pyenv virtualenv 3.6.7 pyenv-jupyter
 pyenv activate pyenv-jupyter
 
 PIP_CMD="pip3 --no-cache-dir install --upgrade"
 $PIP_CMD pip==18.1
-
-$PIP_CMD \
-jaydebeapi \
-jupyterlab \
-psycopg2-binary \
-requests \
-seaborn \
-openpyxl \
-xlrd \
-tqdm \
-sas7bdat \
-sqlalchemy \
-scikit-learn \
-nltk \
-tornado=5.1.1 \
-pandas
+$PIP_CMD $(cat packages)
 
 # Important: rehash pyenv to make jupyter command available
 pyenv rehash
 
-jupyter serverextension enable --py jupyterlab --sys-prefix
-#jupyter labextension install jupyterlab_vim
+#jupyter serverextension enable --py jupyterlab --sys-prefix
+jupyter labextension install jupyterlab_vim
 
 pyenv rehash
 pyenv deactivate
